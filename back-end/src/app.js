067cb527fed5cb7e08658f906cb0f2b00ed749e6
Guilherme Vianna <guilherme.mataveli@miortec.com.br>
2023-06-25 15:42:53 -0300

    release: v0.1

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,9 +1,18 @@
 import { connection } from "./infra/sqlconnection.js";    
 import express from 'express'; 
+import cors from 'cors'
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
 
 const app = express(); 
 const port = 3000;
 
+app.use(cors(corsOptions))
 app.use(express.json());
 
 
@@ -16,10 +25,16 @@ app.post('/questions', async (req, res) => {
   return res.status(201).json(newQuestion)
 }); 
 
+// app.get('/questions', async (req, res) => { 
+//   let [result] = await connection.execute('SELECT * FROM Questions');  
+//   return res.status(200).json(result)
+// }); 
+
 app.get('/questions', async (req, res) => { 
-  let [result] = await connection.execute('SELECT * FROM Questions');  
+  let [result] = await connection.execute('SELECT * FROM Questions ORDER BY RAND() LIMIT 1');  
   return res.status(200).json(result)
 }); 
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
