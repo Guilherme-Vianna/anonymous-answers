881d568c81da358abacea6f0dc3a8a08921295aa
AlehandroSV <alehandrosantos99@gmail.com>
2023-07-03 14:35:45 -0300

    feat: Liberando CORS e ajustando .gitignore

--- src/app.js
+++ src/app.js
@@ -1,41 +1,58 @@
-import { connection } from "./infra/sqlconnection.js";    
-import express from 'express'; 
-import cors from 'cors'
+import { connection } from "./infra/sqlconnection.js";
+import express from "express";
+import cors from "cors";
 
-const app = express(); 
+const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use((req, res, next) => {
-  console.log("Acessou o Middleware!");
-  req.header("Access-Control-Allow-Origin", "*");
-  req.header("Acess-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  app.use(cors()); 
-  next(); 
+
+app.use(cors());
+app.options({
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
 });
-app.post('/questions', async (req, res) => { 
+
+app.post("/questions", async (req, res) => {
   console.log(req.body);
   const [newQuestion] = await connection.execute(
-    'INSERT INTO Questions(question, alternative1, alternative2, alternative3, answer)'+ 
-    'VALUES (?,?,?,?,?)', [req.body.question, req.body.alt1, req.body.alt2, req.body.alt3, req.body.ans]);
+    "INSERT INTO Questions(question, alternative1, alternative2, alternative3, answer)" +
+      "VALUES (?,?,?,?,?)",
+    [
+      req.body.question,
+      req.body.alt1,
+      req.body.alt2,
+      req.body.alt3,
+      req.body.ans,
+    ]
+  );
 
-  return res.status(201).json(newQuestion)
-}); 
+  return res.status(201).json(newQuestion);
+});
 
-app.get('/questions', async (req, res) => { 
-  let [result] = await connection.execute('SELECT * FROM Questions ORDER BY RAND() LIMIT 1');  
-  return res.status(200).json(result)
-}); 
+app.get("/questions", async (req, res) => {
+  let [result] = await connection.execute(
+    "SELECT * FROM Questions ORDER BY RAND() LIMIT 1"
+  );
+  return res.status(200).json(result);
+});
 
-app.post('/questions/create', async (req, res) => { 
-  const query = 'INSERT INTO Questions(question, alternative1, alternative2, alternative3, answer) VALUES (?,?,?,?,?)'; 
-  const body = req.body; 
-  let [result] = await connection.execute(query, [body.question, body.alt1, body.alt2, body.alt3, body.ans]) ;  
-  return res.status(200).json(result)
-}); 
-
+app.post("/questions/create", async (req, res) => {
+  const query =
+    "INSERT INTO Questions(question, alternative1, alternative2, alternative3, answer) VALUES (?,?,?,?,?)";
+  const body = req.body;
+  let [result] = await connection.execute(query, [
+    body.question,
+    body.alt1,
+    body.alt2,
+    body.alt3,
+    body.ans,
+  ]);
+  return res.status(200).json(result);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})  
-
+  console.log(`Example app listening on port ${port}`);
+});
--- src/infra/sqlconnection.js
+++ src/infra/sqlconnection.js
@@ -1,12 +1,11 @@
-import mysql from 'mysql2/promise'; 
+import mysql from "mysql2/promise";
 
 const connection = mysql.createPool({
-	host: 'containers-us-west-123.railway.app',
-	user: 'root',
-	database: 'railway',
-	password: '3QmA18nKavKXiPOkBnZZ',
-	port: '6350'
-}); 
+  host: "containers-us-west-123.railway.app",
+  user: "root",
+  database: "railway",
+  password: "3QmA18nKavKXiPOkBnZZ",
+  port: "6350",
+});
 
-export {connection}; 
-
+export { connection };
