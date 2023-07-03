085ded5c37483df722d24315ca9da1f592da33b3
Guilherme-Vianna <mataveli91@gmail.com>
2023-07-03 13:43:41 -0300

    fix: Another cors change

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ const port = 3000;
 app.use(express.json());
 app.use((req, res, next) => {
   console.log("Acessou o Middleware!");
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Acess-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  req.header("Access-Control-Allow-Origin", "*");
+  req.header("Acess-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   app.use(cors()); 
   next(); 
 });
