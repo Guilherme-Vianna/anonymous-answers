6359ec59f3c2af11dfa573b514b7ee57cdbfb914
Guilherme-Vianna <mataveli91@gmail.com>
2023-07-03 09:59:56 -0300

    Front End removed

--- front-end/pages/script/playmode.js
+++
@@ -1,7 +0,0 @@
-const playButton = document.getElementById("play")
-playButton.addEventListener("click", Play);
-
-function Play(){ 
-  console.log("Vamos responder");
-  window.location.replace("http://localhost:5500/front-end/pages/answer.html"); 
-}
