ba09794866695c028f09217d5d677a74ec463506
Guilherme Vianna <guilherme.mataveli@miortec.com.br>
2023-07-06 21:26:14 -0300

    feat: Cors liberados.

--- src/app.js
+++ src/app.js
@@ -5,15 +5,8 @@ import cors from "cors";
 const app = express();
 const port = process.env.PORT || 3000
 
-app.use(express.json());
-
 app.use(cors());
-app.options({
-  origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-});
+app.use(express.json());
 
 app.post("/questions", async (req, res) => {
   console.log(req.body);
